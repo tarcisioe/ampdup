488a60d6d1c32e5b29c6bc77dd77ff43588bf488
Tarcisio Eduardo Moreira Crocomo <tarcisio.crocomo@gmail.com>
2018-03-09 00:56:21 -0300

    Clean up the internal typing module.

--- ampdup/typing.py
+++ ampdup/typing.py
@@ -1,86 +1,20 @@
 # pylint: skip-file
 
-from abc import abstractproperty, abstractmethod
+from abc import abstractmethod
 from typing import AnyStr, Generic, List, Union
 
 
 class AsyncIO(Generic[AnyStr]):
-    """Generic base class for TextIO and BinaryIO.
-
-    This is an abstract, generic version of the return of open().
-
-    NOTE: This does not distinguish between the different possible
-    classes (text vs. binary, read vs. write vs. read/write,
-    append-only, unbuffered).  The TextIO and BinaryIO subclasses
-    below capture the distinctions between text vs. binary, which is
-    pervasive in the interface; however we currently do not offer a
-    way to track the other distinctions in the type system.
-    """
+    """Generic base class for async TextIO and BinaryIO."""
 
     __slots__ = ()
-
-    @abstractproperty
-    def mode(self) -> str:
-        pass
-
-    @abstractproperty
-    def name(self) -> str:
-        pass
-
-    @abstractmethod
-    def close(self) -> None:
-        pass
-
-    @abstractmethod
-    def closed(self) -> bool:
-        pass
-
-    @abstractmethod
-    def fileno(self) -> int:
-        pass
-
-    @abstractmethod
-    def flush(self) -> None:
-        pass
-
-    @abstractmethod
-    def isatty(self) -> bool:
-        pass
-
-    @abstractmethod
-    async def read(self, n: int = -1) -> AnyStr:
-        pass
-
-    @abstractmethod
-    def readable(self) -> bool:
-        pass
 
     @abstractmethod
     async def readline(self, limit: int = -1) -> AnyStr:
         pass
 
     @abstractmethod
-    def readlines(self, hint: int = -1) -> List[AnyStr]:
-        pass
-
-    @abstractmethod
-    def seek(self, offset: int, whence: int = 0) -> int:
-        pass
-
-    @abstractmethod
-    def seekable(self) -> bool:
-        pass
-
-    @abstractmethod
-    def tell(self) -> int:
-        pass
-
-    @abstractmethod
-    def truncate(self, size: int = None) -> int:
-        pass
-
-    @abstractmethod
-    def writable(self) -> bool:
+    async def readlines(self, hint: int = -1) -> List[AnyStr]:
         pass
 
     @abstractmethod
@@ -88,20 +22,20 @@ class AsyncIO(Generic[AnyStr]):
         pass
 
     @abstractmethod
-    def writelines(self, lines: List[AnyStr]) -> None:
+    async def writelines(self, lines: List[AnyStr]) -> None:
         pass
 
     @abstractmethod
-    def __aenter__(self) -> 'AsyncIO[AnyStr]':
+    async def __aenter__(self) -> 'AsyncIO[AnyStr]':
         pass
 
     @abstractmethod
-    def __aexit__(self, type, value, traceback) -> None:
+    async def __aexit__(self, type, value, traceback) -> None:
         pass
 
 
 class AsyncBinaryIO(AsyncIO[bytes]):
-    """Typed version of the return of open() in binary mode."""
+    """Typed version of an async IO object, counterparte to typing.BinaryIO."""
 
     __slots__ = ()
 
@@ -110,5 +44,5 @@ class AsyncBinaryIO(AsyncIO[bytes]):
         pass
 
     @abstractmethod
-    def __aenter__(self) -> 'AsyncBinaryIO':
+    async def __aenter__(self) -> 'AsyncBinaryIO':
         pass
