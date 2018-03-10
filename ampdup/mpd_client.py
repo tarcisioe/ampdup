afae60c40e9746288748266433a38d8a878934d7
Tarcisio Eduardo Moreira Crocomo <tarcisio.crocomo@gmail.com>
2018-03-10 02:04:29 -0300

    Create currentsong commmand and handle its format..

--- ampdup/mpd_client.py
+++ ampdup/mpd_client.py
@@ -1,13 +1,48 @@
 '''MPD Client module.'''
+from typing import NamedTuple
+
 from .base_client import BaseMPDClient
 from .errors import ClientTypeError
-from .util import has_any_prefix
+from .util import has_any_prefix, from_json_like
+
+
+class Song(NamedTuple):
+    '''Type representing the static data about a playable song in MPD.'''
+    file: str
+    last_modified: str
+    artist: str
+    title: str
+    album: str
+    track: int
+    date: int
+    genre: str
+    time: int
+    duration: float
+    pos: int
+    id: int
+
+
+def normalize(s: str) -> str:
+    '''normalize'''
+    return s.lower().replace('-', '_')
 
 
 class MPDClient(BaseMPDClient):
     '''An async MPD Client object for any operations except idle/noidle.'''
+
     async def run_command(self, command: str):
         if has_any_prefix(command, ('idle', 'noidle')):
             raise ClientTypeError('Use an IdleClient to use the idle command.')
 
         return await super().run_command(command)
+
+    async def current_song(self) -> Song:
+        '''Displays the song info of the current song.
+
+        Returns:
+            Song: The same song that is identified in status.
+        '''
+        result = await self.run_command('currentsong')
+        values = (r.split(': ') for r in result)
+        normalized = {normalize(k): v for k, v in values}
+        return from_json_like(Song, normalized)
