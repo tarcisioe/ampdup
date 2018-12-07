60e90b1ce8d010552ad1a39153aa777f2f9a3945
Tarcisio Eduardo Moreira Crocomo <tarcisio.crocomo@gmail.com>
2018-12-07 01:43:20 -0200

    Better handling of reconnection.

--- ampdup/base_client.py
+++ ampdup/base_client.py
@@ -2,9 +2,9 @@ from typing import AsyncGenerator, List, Optional, Union, Tuple
 
 from dataclasses import dataclass, field
 
-from asyncio import Future, AbstractEventLoop  # pylint: disable=unused-import
-from asyncio import Queue
-from asyncio import get_event_loop, Task, CancelledError
+from asyncio import (  # pylint:disable=unused-import
+    create_task, get_running_loop, CancelledError, Future, Queue, Task
+)
 
 from .connection import Connection
 from .errors import CommandError, ConnectionFailedError
@@ -13,46 +13,31 @@ from .util import asynccontextmanager
 
 
 @dataclass
-class BaseMPDClient:
+class MPDConnection:
     pending_commands: 'Queue[Future[List[str]]]' = field(default_factory=Queue)
     connection: Optional[Connection] = None
     loop: Optional[Task] = None
-    details: Optional[Tuple[str, int]] = None
-    event_loop: AbstractEventLoop = field(
-        default_factory=get_event_loop
-    )
-
-    @classmethod
-    @asynccontextmanager
-    async def make(cls, address: str, port: int) -> AsyncGenerator:
-        c = cls()
-        await c.connect(address, port)
-        yield c
-        await c.disconnect()
 
     async def connect(self, address: str, port: int):
         self.connection = Connection()
         await self.connection.connect(address, port)
-        self.details = (address, port)
         self._start_loop()
 
     async def disconnect(self):
-        await self._clear_connection()
-        if self.loop:
-            self.loop.cancel()
-        self.loop = None
+        if self.connection is not None:
+            await self.connection.close()
 
-    async def reconnect(self):
-        await self.disconnect()
-        if self.details is None:
-            raise ConnectionFailedError('Not previously connected.')
-        await self.connect(*self.details)
+        if self.loop is not None:
+            self.loop.cancel()
+
+        self.loop = None
+        self.connection = None
 
     async def run_command(self, command: str) -> List[str]:
         if self.connection is None:
             raise ConnectionFailedError()
 
-        p: 'Future[List[str]]' = self.event_loop.create_future()
+        p: 'Future[List[str]]' = get_running_loop().create_future()
         await self.pending_commands.put(p)
 
         await self.connection.write_line(command)
@@ -79,25 +64,63 @@ class BaseMPDClient:
 
         return lines
 
-    async def _clear_connection(self):
-        if self.connection is not None:
-            await self.connection.close()
-            self.connection = None
+    def _start_loop(self):
+        self.loop = create_task(self._reply_loop())
 
     async def _reply_loop(self):
         try:
             while True:
+                pending = await self.pending_commands.get()
                 try:
                     reply = await self._read_response()
-                except Exception:  # pylint: disable=broad-except
-                    await self._clear_connection()
-                    self.loop = None
-                    return
+                except CancelledError:
+                    pending.set_exception(ConnectionFailedError())
+                    raise
+                except Exception as e:  # pylint: disable=broad-except
+                    pending.set_exception(e)
                 else:
-                    pending = await self.pending_commands.get()
                     pending.set_result(reply)
+                self.pending_commands.task_done()
         except CancelledError:
             return
 
-    def _start_loop(self):
-        self.loop = self.event_loop.create_task(self._reply_loop())
+
+@dataclass
+class BaseMPDClient:
+    connection: Optional[MPDConnection] = None
+    details: Optional[Tuple[str, int]] = None
+
+    @classmethod
+    @asynccontextmanager
+    async def make(cls, address: str, port: int) -> AsyncGenerator:
+        c = cls()
+        await c.connect(address, port)
+        yield c
+        await c.disconnect()
+
+    async def connect(self, address: str, port: int):
+        self.connection = MPDConnection()
+
+        await self.connection.connect(address, port)
+        self.details = (address, port)
+
+    async def disconnect(self):
+        if self.connection is not None:
+            await self.connection.disconnect()
+
+    async def reconnect(self):
+        if self.connection is None or self.details is None:
+            raise ConnectionFailedError(
+                'Cannot reconnect if not previously connected.'
+            )
+
+        await self.disconnect()
+        await self.connect(*self.details)
+
+    async def run_command(self, command: str) -> List[str]:
+        if self.connection is None:
+            raise ConnectionFailedError(
+                'Connection is not established.'
+            )
+
+        return await self.connection.run_command(command)
