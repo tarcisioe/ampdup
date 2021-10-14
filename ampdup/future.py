410101dd08889e1a81d0d32f2ca8f7e4e1de5682
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2021-10-14 00:44:21 -0300

    Change Future implementation to a state-based one, finish testing.

--- ampdup/future.py
+++ ampdup/future.py
@@ -1,11 +1,9 @@
 """Implementation of a loop-agnostic Future class."""
 
 from dataclasses import dataclass, field
-from typing import Generic, TypeVar, Union
+from typing import Generic, Protocol, TypeVar
 
 from anyio import Event
-
-from .maybe import Maybe, Nothing
 
 T = TypeVar('T')
 
@@ -18,21 +16,116 @@ class FutureValueError(Exception):
     """Signal an attempt to set a Future to an unsupported value."""
 
 
-@dataclass
-class Future(Generic[T]):
-    """A class wrapping a value that may not yet have been set."""
+class PendingFutureError(Exception):
+    """Signal an attempt to get directly from a pending future."""
+
+
+@dataclass(frozen=True)
+class StateChange(Generic[T]):
+    """Information generated when a future changes state."""
+
+    next_state: 'FutureState[T]'
+    event: Event
+
+
+class FutureState(Generic[T], Protocol):
+    """The internal state of a Future."""
+
+    def set(self, value: T) -> StateChange[T]:
+        """Produce a new state when the future is set from this given state.
+
+        When the state changes, an event must be provided to signal tasks that are
+        waiting for the future to be set.
+        """
+        ...
+
+    def fail(self, error: Exception) -> StateChange[T]:
+        """Produce a new state when the future is failed from this given state."""
+        ...
+
+    async def wait_fulfilled(self) -> None:
+        """Wait until the future is fulfilled."""
+        ...
+
+    def get(self) -> T:
+        """Get the value of the future if available in the state."""
+        ...
+
+
+@dataclass(frozen=True)
+class BaseSetFuture(Generic[T]):
+    """Base for a future state that has something set."""
+
+    @staticmethod
+    def set(value: T) -> StateChange[T]:
+        """Fails since in this state the future is already set."""
+        raise FutureAlreadySetError('Future cannot be reset.')
+
+    @staticmethod
+    def fail(error: Exception) -> StateChange[T]:
+        """Fails since in this state the future is already set."""
+        raise FutureAlreadySetError('Future cannot be reset.')
+
+    @staticmethod
+    async def wait_fulfilled() -> None:
+        """There is nothing to wait since the future is already set."""
+
+
+@dataclass(frozen=True)
+class SuccessfulFuture(BaseSetFuture[T]):
+    """The state of a successful Future (a value has been set)."""
+
+    value: T
+
+    def get(self) -> T:
+        """Return the set value."""
+        return self.value
+
+
+@dataclass(frozen=True)
+class FailedFuture(BaseSetFuture[T]):
+    """The state of a failed Future (an Exception has been set)."""
+
+    error: Exception
+
+    def get(self) -> T:
+        """Raise the set error."""
+        raise self.error
+
+
+@dataclass(frozen=True)
+class PendingFuture(Generic[T]):
+    """The state of a pending Future."""
 
     _fulfilled: Event = field(default_factory=Event, init=False)
-    _value: Maybe[Union[T, Exception]] = field(default=Nothing, init=False)
 
-    def _check_already_set(self):
-        if self._fulfilled.is_set():
-            raise FutureAlreadySetError('Future cannot be reset.')
+    def set(self, value: T) -> StateChange[T]:
+        """Produce a new SuccessFuture state with the given value."""
+        return StateChange(SuccessfulFuture(value), self._fulfilled)
 
-    def _set_value(self, value: Union[T, Exception]) -> None:
-        self._check_already_set()
-        self._value = value
-        self._fulfilled.set()
+    def fail(self, error: Exception) -> StateChange[T]:
+        """Produce a new FailedFuture state with the given error."""
+        return StateChange(FailedFuture(error), self._fulfilled)
+
+    async def wait_fulfilled(self) -> None:
+        """Wait until the future is fulfilled (goes through a state change)."""
+        await self._fulfilled.wait()
+
+    @staticmethod
+    def get() -> T:  # pragma: no cover
+        """Fail since the future is still pending. Should never be called."""
+        raise ValueError('Future is pending.')
+
+
+@dataclass
+class Future(Generic[T]):
+    """A class wrapping a value that may not yet have been set.
+
+    T must not be derived from Exception. There seems to be no way to enforce
+    this through static typing yet.
+    """
+
+    _state: FutureState[T] = field(default_factory=PendingFuture, init=False)
 
     def set(self, value: T) -> None:
         """Set the value of the future to a success value.
@@ -40,11 +133,9 @@ class Future(Generic[T]):
         Args:
             value: The value to set the Future to. Cannot be an Exception.
         """
-        if isinstance(value, Exception):
-            raise FutureValueError(
-                'Cannot set the value of a future to an Exception. Use .fail() instead.'
-            )
-        self._set_value(value)
+        state_change = self._state.set(value)
+        self._state = state_change.next_state
+        state_change.event.set()
 
     def fail(self, error: Exception) -> None:
         """Signal failure and cause calls to `get()` to raise.
@@ -52,17 +143,14 @@ class Future(Generic[T]):
         Args:
             error: The exception that should be raised when `get()` is called.
         """
-        self._set_value(error)
+        state_change = self._state.fail(error)
+        self._state = state_change.next_state
+        state_change.event.set()
 
     async def get(self) -> T:
         """Get the value from the future asynchronously.
 
         If the future was set by `fail()`, raises the exception.
         """
-        await self._fulfilled.wait()
-        assert self._value is not Nothing
-
-        if isinstance(self._value, Exception):
-            raise self._value
-
-        return self._value
+        await self._state.wait_fulfilled()
+        return self._state.get()
--- tests/test_future.py
+++ tests/test_future.py
@@ -5,7 +5,30 @@ from ampdup.future import Future, FutureAlreadySetError
 
 
 @pytest.mark.anyio
-async def test_set_future_twice():
+async def test_get_set_future() -> None:
+    """Checks that a set future returns the set value."""
+    f: Future[int] = Future()
+
+    f.set(1)
+    assert await f.get() == 1
+
+
+@pytest.mark.anyio
+async def test_get_failed_future() -> None:
+    """Checks that a failed future raises."""
+    f: Future[int] = Future()
+
+    class DummyError(Exception):
+        """Error class for testing."""
+
+    f.fail(DummyError())
+
+    with pytest.raises(DummyError):
+        await f.get()
+
+
+@pytest.mark.anyio
+async def test_set_future_twice() -> None:
     """Check that setting a future twice fails."""
     f: Future[int] = Future()
 
@@ -13,3 +36,57 @@ async def test_set_future_twice():
 
     with pytest.raises(FutureAlreadySetError):
         f.set(1)
+
+
+@pytest.mark.anyio
+async def test_fail_set_future() -> None:
+    """Check that failing a set future fails."""
+    f: Future[int] = Future()
+
+    f.set(1)
+
+    with pytest.raises(FutureAlreadySetError):
+        f.fail(ValueError('Oh no!'))
+
+
+@pytest.mark.anyio
+async def test_set_failed_future() -> None:
+    """Check that setting a failed future fails."""
+    f: Future[int] = Future()
+
+    f.fail(ValueError('Oh no!'))
+
+    with pytest.raises(FutureAlreadySetError):
+        f.set(1)
+
+
+@pytest.mark.anyio
+async def test_fail_future_twice() -> None:
+    """Check that failing a future twice fails."""
+    f: Future[int] = Future()
+
+    f.fail(ValueError('Oh no!'))
+
+    with pytest.raises(FutureAlreadySetError):
+        f.fail(ValueError('Oh no!'))
+
+
+@pytest.mark.anyio
+async def test_future_set_after_get() -> None:
+    """Check that setting a future after a call to get works, with multiple getters."""
+    from anyio import Event, create_task_group
+
+    will_get = Event()
+
+    async def get_future(f: Future[int]) -> None:
+        will_get.set()
+        assert await f.get() == 3
+
+    f: Future[int] = Future()
+
+    async with create_task_group() as tg:
+        tg.start_soon(get_future, f)
+        tg.start_soon(get_future, f)
+        tg.start_soon(get_future, f)
+        await will_get.wait()
+        f.set(3)
